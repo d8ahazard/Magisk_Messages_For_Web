e31bd0ec898c4188f5d329463e080670fc40c4ae
d8ahazard <d8ahazard@gmail.com>
2018-06-23 23:48:41 -0500

    Initial commit
    Script works, but it's not executing properly...

---
+++ README.md
@@ -0,0 +1,5 @@
+#Android Messages for Web Enabler
+
+This just enables a few settings flags needed for MFW to be linkable and stay enabled.
+
+Tested only on the Pixel XL, Mileage on other devices will definitely vary.
---
+++ config.sh
@@ -0,0 +1,89 @@
+##########################################################################################
+#
+# Magisk Module Template Config Script
+# by topjohnwu
+#
+##########################################################################################
+##########################################################################################
+#
+# Instructions:
+#
+# 1. Place your files into system folder (delete the placeholder file)
+# 2. Fill in your module's info into module.prop
+# 3. Configure the settings in this file (config.sh)
+# 4. If you need boot scripts, add them into common/post-fs-data.sh or common/service.sh
+# 5. Add your additional or modified system properties into common/system.prop
+#
+##########################################################################################
+
+##########################################################################################
+# Configs
+##########################################################################################
+
+# Set to true if you need to enable Magic Mount
+# Most mods would like it to be enabled
+AUTOMOUNT=true
+
+# Set to true if you need to load system.prop
+PROPFILE=false
+
+# Set to true if you need post-fs-data script
+POSTFSDATA=false
+
+# Set to true if you need late_start service script
+LATESTARTSERVICE=true
+
+##########################################################################################
+# Installation Message
+##########################################################################################
+
+# Set what you want to show when installing your mod
+
+print_modname() {
+  ui_print " "
+  ui_print "    *****************************************"
+  ui_print "    *   Android Messages for Web Enabler    *"
+  ui_print "    *****************************************"
+  ui_print "    *                  v1.0                 *"
+  ui_print "    *               Digtialhigh             *"
+  ui_print "    *******************************************"
+  ui_print " "
+}
+
+##########################################################################################
+# Replace list
+##########################################################################################
+
+# List all directories you want to directly replace in the system
+# Check the documentations for more info about how Magic Mount works, and why you need this
+
+# This is an example
+REPLACE="
+/system/app/Youtube
+/system/priv-app/SystemUI
+/system/priv-app/Settings
+/system/framework
+"
+
+# Construct your own list here, it will override the example above
+# !DO NOT! remove this if you don't need to replace anything, leave it empty as it is now
+REPLACE="
+"
+
+##########################################################################################
+# Permissions
+##########################################################################################
+
+set_permissions() {
+  # Only some special files require specific permissions
+  # The default permissions should be good enough for most cases
+
+  # Here are some examples for the set_perm functions:
+
+  # set_perm_recursive  <dirname>                <owner> <group> <dirpermission> <filepermission> <contexts> (default: u:object_r:system_file:s0)
+  set_perm $UNITY$SYS/bin/sqlite3 0 0 0755
+   # The following is default permissions, DO NOT remove
+  set_perm_recursive  $MODPATH  0  0  0755  0644
+  set_perm $MODPATH/service.sh 0 0 0755
+
+}
